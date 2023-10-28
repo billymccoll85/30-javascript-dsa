9e0cbad1b48e0c31eef2000211e58d9f7f7320fd
CodementorUK <billymccoll2024@gmail.com>
2023-10-28 12:16:28 +0100

    simplifed version of link lists

--- 03.linked-lists.js
+++ 03.linked-lists.js
@@ -1,55 +1,42 @@
-// Creating a linked list node
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
-
-// Creating a linked list
 class LinkedList {
   constructor() {
-    this.head = null;
+      this.head = null;
   }
 
   // Adding a node to the end of the linked list
   addNode(data) {
-    let newNode = new Node(data);
+      let newNode = { data: data, next: null };
 
-    if (this.head === null) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-
-      while (current.next !== null) {
-        current = current.next;
+      if (this.head === null) {
+          this.head = newNode;
+      } else {
+          let current = this.head;
+          while (current.next !== null) {
+              current = current.next;
+          }
+          current.next = newNode;
       }
-
-      current.next = newNode;
-    }
   }
 
   // Removing a node from the linked list
   removeNode(data) {
-    if (this.head === null) {
-      return;
-    }
-
-    if (this.head.data === data) {
-      this.head = this.head.next;
-      return;
-    }
-
-    let current = this.head;
-
-    while (current.next !== null) {
-      if (current.next.data === data) {
-        current.next = current.next.next;
-        return;
+      if (this.head === null) {
+          return;
       }
 
-      current = current.next;
-    }
+      if (this.head.data === data) {
+          this.head = this.head.next;
+          return;
+      }
+
+      let current = this.head;
+      while (current.next !== null) {
+          if (current.next.data === data) {
+              current.next = current.next.next;
+              return;
+          }
+          current = current.next;
+      }
   }
 }
 
@@ -58,8 +45,8 @@ let ll = new LinkedList();
 ll.addNode(1);
 ll.addNode(2);
 ll.addNode(3);
-console.log(ll); // Output: LinkedList { head: Node { data: 1, next: Node { data: 2, next: Node { data: 3, next: null } } } }
+console.log(ll);  // Output: { head: { data: 1, next: { data: 2, next: { data: 3, next: null } } } }
 
 // Removing a node from the linked list
 ll.removeNode(2);
-console.log(ll); // Output: LinkedList { head: Node { data: 1, next: Node { data: 3, next: null } } }
+console.log(ll);  // Output: { head: { data: 1, next: { data: 3, next: null } } }
